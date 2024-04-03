5beb409ceb447c9e50869c9db9e2690fa4f2e65f
mohammedabdelaleem <mohammedabdelaleem20@gmail.com>
2024-04-03 16:59:25 +0200

    Thanks Allah I Toke the idea and go ahead to analyse and coding. now i'll go th the instructor's code.the idea is a password manager , user has 5 choices 1-list all his passwords 2-add new password 3-change the existiing password 4-get the password by key 5-remove the password and exit .the idea was very simple but i made it bigger and bigger for learning and deals with files and so on......

---
+++ CSharp-projects/PasswordManager/Program.cs
@@ -0,0 +1,365 @@
+﻿// the src: https://www.youtube.com/watch?v=ERh-5JkHdRQ&t=17s
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PasswordManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+            Console.ReadLine();
+        }
+
+
+        private static void Start()
+        {
+            do
+            {
+                Console.Clear();
+                Loading("Password Manager Project");
+                PrintTheMainList();
+                PerformUserChoice((enChoices)ReadChoice(0, 5));
+
+            } while (true);
+        }
+
+        public enum enChoices { EXIT=0 ,listAllPasswords =1 , AddNewPassword=2 , ChangePassword=3 ,
+            GetPassword=4 , RemovePassword=5
+        }
+     
+       public static Dictionary<string, string> dictPasswords  = new Dictionary<string, string>();
+        const string path = "D:\\University\\Coding\\Basic\\Basic-C#\\Projects\\PasswordManager\\myPasswords.txt";
+        public static byte ReadChoice(byte from, byte to)
+        {
+            byte choice = default(byte);
+            bool isNotValid = true;
+
+            Console.Write($"Enter Your Choice[{from} - {to}] : ");
+            do
+            {
+                isNotValid = ! byte.TryParse(Console.ReadLine(), out choice) || choice < from || choice > to;
+                if (isNotValid)
+                {
+                    Console.Write($"Please Enter A Valid Choice [{from} - {to}]\a : ");
+                }
+
+            } while (isNotValid);
+            return choice;
+        }
+
+        public static string GetNumOfSpace(int num)
+        {
+            string spaces = "";
+            for (int i = 0; i < num; i++)
+                spaces += " ";
+           
+            return spaces;
+        }
+        public static void Loading(string message)
+        {
+            int strLen = message.Length;
+            for (int i = 1; i <=4; i++)
+            {
+                if(i<4)
+               Console.Write(message+"\r");
+                else
+                    Console.WriteLine(message );
+                message += "."+ GetNumOfSpace(strLen/10); // by trial
+                Thread.Sleep(800);
+              //  Console.Clear();
+            }
+            Console.WriteLine("--------------------------");
+        }
+
+        public static void PrintTheMainList()
+        {
+            Console.WriteLine("1- List All Passwords.");
+            Console.WriteLine("2- Add New Password.");
+            Console.WriteLine("3- Change Password.");
+            Console.WriteLine("4- Get Password.");
+            Console.WriteLine("5- Remove Password.");
+            Console.WriteLine("0- EXIT.");
+            Console.WriteLine("----------------------------\n");
+
+        }
+
+        public static void PressAnyKeyToContinue()
+        {
+            Console.WriteLine("\n\n\nPress Enter To Continue........");
+            Console.Read();
+        }
+        public static void PerformUserChoice(enChoices choice)
+        {
+            switch(choice) {
+                case enChoices.listAllPasswords:
+                    ListAllPasswords(path);
+                    PressAnyKeyToContinue();
+                    break;
+
+                case enChoices.AddNewPassword:
+                    AddNewPassword();
+                    PressAnyKeyToContinue();
+                    break;
+
+                case enChoices.ChangePassword:
+                    ChangePassword();
+                    PressAnyKeyToContinue();
+                    break;
+
+                case enChoices.GetPassword:
+                    GetPassword();
+                    PressAnyKeyToContinue();
+                    break;
+
+                case enChoices.RemovePassword:
+                    RemovePassword();
+                    PressAnyKeyToContinue();
+                    break;
+                case enChoices.EXIT:
+                    Environment.Exit(0);// Exit the application with exit code 0 (success)
+                    break;
+            }
+        }
+
+        public static string ReadDictonaryKey()
+        {
+            string key;
+            bool wrongKey = false;
+            dictPasswords = GetPasswordsFromFileAsDictonary(path);
+            Console.Write("Please Enter A Key : ");
+            do
+            {
+                key = Console.ReadLine();
+                if (dictPasswords.ContainsKey(key))
+                {
+                    Console.Write("This Key Is Found, Enter Another One : ");
+                    wrongKey = true;
+                }else
+                    wrongKey = false;
+            } while (wrongKey);
+
+            return key;
+        }
+
+        public static string GetExistingDictonaryKeyIfFound()
+        {
+     //       dictPasswords = GetPasswordsFromFile(path);
+            string key;
+            Console.Write("Please Enter A Key : ");
+            do
+            {
+                 key = Console.ReadLine();
+                if (dictPasswords.ContainsKey(key) )
+                {
+                    break;
+                }
+                else
+                {
+                    Console.Write($"{key} Not Found ,Please Enter A Valid Key : ");
+                    continue;
+                }
+            }while(true);
+
+             return key;
+
+        }
+
+        public static string ConvertKeyValueStringsToRecord(string key, string value) =>  key + ":" + value;
+
+        public static string[] GetAllPasswordsFromDictionaryPasswordsAsStringRecords()
+        {
+            string[] Keys = dictPasswords.Keys.ToArray();
+            int len = Keys.Length;
+            string[] Records = new string[len];
+            string Record;
+            for (int i = 0; i < len; i++)
+            {
+                Record = ConvertKeyValueStringsToRecord(Keys[i], dictPasswords[Keys[i]]);
+                Records[i] = Record;
+            }
+            return Records;
+        }
+
+
+        private static void UpdatePasswordsFile(string path)
+        {
+            try
+            {
+                string[] Records = GetAllPasswordsFromDictionaryPasswordsAsStringRecords();
+                File.WriteAllLines(path, Records);
+                Console.WriteLine("\n\n\nFile Updated Successfully.........");
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        public static void  AddNewRecordToPasswordsFile(string path,string keyValuePair)
+        {
+            try
+            {
+                using (StreamWriter writer = File.AppendText(path))
+                {
+                    if(dictPasswords.Count == 0)
+                         writer.WriteLine("\n"+ keyValuePair );
+                    else
+                        writer.WriteLine( keyValuePair );
+
+                }
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+        }
+        public static Dictionary<string, string> GetPasswordsFromFileAsDictonary(string path)
+        {
+            try
+            {
+                dictPasswords.Clear();
+                string[] passwords = File.ReadAllLines(path);
+                string[] currentPassword = new string[2];
+                foreach (string password in passwords)
+                {
+                    currentPassword =  password.Split(':');
+                    dictPasswords.Add( currentPassword[0], currentPassword[1]);
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            return dictPasswords;
+        }
+
+        static string FormatString(string input, int width)
+        {
+            return input.PadRight(width);
+        }
+      
+        private static void ColorTheHeader(string title)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.Yellow;
+            Console.WriteLine(title+"\n\n\n");
+            Console.ResetColor();
+        }
+        public static void PrintPassword(string key)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine("\n");
+            Loading("The Password Is....");
+            Console.WriteLine($"{key} : {dictPasswords[key]}");
+            Console.WriteLine("\n");
+            Console.ResetColor();
+        }
+        private static void ListAllPasswords(string path)
+        {
+            Console.Clear();
+            ColorTheHeader("List All Passwords Screen");
+            dictPasswords = GetPasswordsFromFileAsDictonary(path);
+            string[] Keys= dictPasswords.Keys.ToArray();
+            string[] Values = dictPasswords.Values.ToArray();
+
+            if (Keys.Length > 0) {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("----------------------------------------");
+                Console.WriteLine($"|{FormatString("     Keys", 15)} | {FormatString("     Values", 20)} |");
+                Console.WriteLine("----------------------------------------");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                for (int i = 0; i < Keys.Length; i++)
+                {
+                    Console.WriteLine($"| {FormatString(Keys[i],15)} | {FormatString(Values[i], 20)}|");
+                    Console.WriteLine("----------------------------------------");
+                }
+            }
+            else
+                Console.WriteLine("No Passwords Found\a");
+        }
+        private static void AddNewPassword()
+        {
+            Console.Clear();
+                      ColorTheHeader("Add New Password Screen");
+                        string key = ReadDictonaryKey();
+
+            Console.Write("Enter A Value : ");
+            string value = Console.ReadLine();
+            dictPasswords.Add(key, value);
+            string keyValuePair = ConvertKeyValueStringsToRecord(key, value);
+            AddNewRecordToPasswordsFile(path, keyValuePair); //appending into file 
+            Console.WriteLine("\n\nAdding new Password Succesfully.....");
+        }
+
+        private static void ChangePassword()
+        {
+            Console.Clear();
+            ColorTheHeader("Change Password Screen");
+
+            string key = GetExistingDictonaryKeyIfFound();
+            PrintPassword(key);
+
+            Console.Write("Are You Sure [Y-N ] ? : ");
+            char answer = char.Parse(Console.ReadLine());
+
+            if (char.ToUpper(answer) == 'Y')
+            {
+                Console.Write("\nEnter A New Value : ");
+                string value = Console.ReadLine();
+                dictPasswords[key] = value;
+                UpdatePasswordsFile(path);
+            }else
+            {
+                Console.WriteLine("\nNo Changes Occured..!...");
+            }
+        }
+
+      
+       private static void GetPassword()
+        {
+            Console.Clear();
+            ColorTheHeader("Change Password Screen");
+
+            dictPasswords = GetPasswordsFromFileAsDictonary(path);
+            string key = GetExistingDictonaryKeyIfFound() ;
+            PrintPassword(key);
+            
+        }
+        private static void RemovePassword()
+        {
+            Console.Clear();
+            ColorTheHeader("Remove Password Screen");
+
+            dictPasswords = GetPasswordsFromFileAsDictonary(path);
+            string key = GetExistingDictonaryKeyIfFound();
+            PrintPassword(key);
+
+            Console.Write("Are You Sure [Y-N ] ? : ");
+            char answer = char.Parse(Console.ReadLine());
+
+            if (char.ToUpper(answer) == 'Y')
+            {
+                dictPasswords.Remove(key);
+                UpdatePasswordsFile(path);
+                Console.WriteLine("\n\n\nPassword Removed Successfully....");
+            }
+            else
+            {
+                Console.WriteLine("\nNo Changes Occured..!...");
+            }
+          
+        }
+
+       
+    }
+}
