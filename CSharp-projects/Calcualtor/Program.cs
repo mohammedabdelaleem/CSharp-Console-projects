f985445b70dbf437acfe655128b84b7021b4b7d3
mohammedabdelaleem <mohammedabdelaleem20@gmail.com>
2024-04-07 03:37:03 +0200

    Simple OOP Calculator Project.

---
+++ CSharp-projects/Calcualtor/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcualtor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            clsCalculator Calculator1 = new clsCalculator();
+
+
+            Calculator1.Clear();
+
+            Calculator1.Add(10);
+            Calculator1.PrintResult();
+
+            Calculator1.Add(100);
+            Calculator1.PrintResult();
+
+            Calculator1.Subtract(20);
+            Calculator1.PrintResult();
+
+            Calculator1.Divide(0);
+            Calculator1.PrintResult();
+
+            Calculator1.Divide(2);
+            Calculator1.PrintResult();
+
+            Calculator1.Multiply(3);
+            Calculator1.PrintResult();
+
+            Calculator1.Clear();
+            Calculator1.PrintResult();
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
---
+++ CSharp-projects/Calcualtor/clsCalculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calcualtor
+{
+    internal class clsCalculator
+    {
+        private double _Result = 0;
+        private double _LastNumber = 0;
+        private string _LastOperation = "Clear";
+        public void Clear()
+        {
+            _LastOperation = "Clear";
+            _Result = 0;
+            _LastNumber = 0;
+        }
+    public void Add(double Number)
+        {
+            _LastOperation = "Adding";
+            _LastNumber = Number;
+            _Result += Number;
+        }
+        public void Subtract(double Number)
+        {
+            _LastOperation = "Subtracting";
+            _LastNumber = Number;
+            _Result -= Number;
+        }
+        public void Multiply(double Number)
+        {
+            _LastOperation = "Multiplying";
+            _LastNumber = Number;
+            _Result *= Number;
+        }
+        public void Divide(double Number)
+        {
+            _LastOperation = "Dividing";
+            _LastNumber = Number;
+            if (Number == 0)
+                Number = 1;
+            _Result /= Number;
+        }
+
+        private double GetFinalResult()
+        {
+            return _Result;
+        }
+
+        public void PrintResult()
+        {
+            Console.WriteLine("Result After " + _LastOperation + " " + _LastNumber + " is : " + GetFinalResult());
+        }
+
+    }
+}
